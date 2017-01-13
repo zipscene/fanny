305df77fe5007335cdaf3e86c21bcd461cc01942
Chris Breneman <crispy@cluenet.org>
2017-01-13 14:24:54 -0500

    Works with arrays

--- src/fanny.cc
+++ src/fanny.cc
@@ -26,6 +26,30 @@ NAN_METHOD(FANN::New) {
 }
 
 NAN_METHOD(FANN::test) {
+	uint32_t numArgs = info.Length();
+	if (numArgs == 0) {
+		std::cout << "No args!\n";
+	} else if (info[0]->IsUndefined()) {
+		std::cout << "Undefined!\n";
+	} else if (info[0]->IsNull()) {
+		std::cout << "Null!\n";
+	} else if (info[0]->IsArray()) {
+		v8::Local<v8::Array> array = v8::Local<v8::Array>::Cast(info[0]);
+		for (uint32_t i = 0; i < array->Length(); i++) {
+			v8::Local<v8::Value> value = array->Get(i);
+			if (value->IsString()) {
+				//v8::Local<v8::String> str = Nan::To<v8::String>(value).ToLocalChecked();
+				v8::String::Utf8Value utf8Str(value);
+				const char * cStr = *utf8Str;
+				std::cout << cStr << ", ";
+			} else {
+				std::cout << "<NotString>, ";
+			}
+		}
+		std::cout << "\n";
+	} else {
+		std::cout << "Unrecognized type\n";
+	}
 	std::cout << "Test!\n";
 }
 
